aab8b3ff51e0147921f8a84f095a04308983f0a2
zhuminjun1983 <sjy87@hotmail.com>
2021-08-24 21:29:38 +0000

    new

---
+++ survey.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  rl.close();
+});
+
+
